8e2fdfbb4715fc71738db32fbcc3afccd22dc36b
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-03-17 11:20:38 +0600

    initial setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,40 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { IconSymbol } from '@/app-example/components/ui/IconSymbol'
+import { SvgXml } from 'react-native-svg'
+import { carIcon } from '../components/icon'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+const TabsLayout = () => {
+    return (
+        <>
+            <StatusBar
+                style="light"
+                backgroundColor="#29adf8"
+                translucent={false}
+                hidden={false}
+            />
+            <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+                <Tabs.Screen 
+                    name="index"  
+                    options={{
+                        tabBarIcon: ({ color }) => (
+                            <SvgXml xml={carIcon}/>
+                        ),
+                    }} 
+                />
+                <Tabs.Screen 
+                    name="user" 
+                    options={{
+                        tabBarIcon: ({ color }) => (
+                            <SvgXml xml={carIcon}/>
+                        ),
+                    }} 
+                />
+            </Tabs>
+        </>
+    )
 }
+
+export default TabsLayout
