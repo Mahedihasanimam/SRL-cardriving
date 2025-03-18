d2789f7f9544d0227a220fdac23245068f858705
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-03-18 14:37:36 +0600

    custom sidebar added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import tw from 'twrnc';
 import { useNavigation } from 'expo-router';
 import { DrawerActions } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
 const Header = () => {
     const navigation = useNavigation();
@@ -11,9 +12,12 @@ const Header = () => {
             <Image source={require('../../assets/images/companylogo_srl.png')} />
 
             <View style={tw`flex-row items-center gap-3`}>
-                <Text style={tw`text-lg font-bold text-white`}>BANINDER PAL</Text>
-                <TouchableOpacity  onPress={() => navigation.dispatch(DrawerActions.openDrawer())} style={tw` border border-white rounded-full px-[6px]`}>
-                    <Text style={tw`text-white text-lg`}>☰</Text>
+                <Text style={tw`text-xl font-bold text-white`}>BANINDER PAL</Text>
+                {/* <TouchableOpacity  onPress={() => navigation.dispatch(DrawerActions.openDrawer())} style={tw` border border-white rounded-full h-[40px] w-[40px] flex flex-row items-center justify-center`}>
+                    <Text style={tw`text-white text-2xl`}>☰</Text>
+                </TouchableOpacity>  */}
+                <TouchableOpacity style={tw`w-10 h-10 rounded-full bg-white bg-opacity-20 items-center justify-center`} onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+                     <Ionicons name="menu" size={24} color="white" />
                 </TouchableOpacity>
             </View>
         </View>
